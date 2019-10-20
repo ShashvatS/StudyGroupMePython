8e2a8caee11fd782c10e2650cfbc6ce036177e04
Shashvat Srivastava <shashvat.sri@gmail.com>
2019-10-19 23:31:35 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+accesstoken = os.getenv("accesstoken")
+
+from groupy import Client
+client = Client.from_token(accesstoken)
+
+def create_group(groupname):
+    new_group = client.groups.create(name=groupname)
+    new_group.update(share=True)
+    for group in client.groups.list_all():
+        if group.name == groupname:
+            new_group = group
+            break
+    return new_group.data["share_url"]
+
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/<group>', methods=['POST'])
+def main(group):
+    print(group)
+    link = create_group(group)
+    result = {}
+    result["group_link"] = link
+    return jsonify(result)
+
+
+### curl -X POST -F 'group=testtesttest" localhost:5000    
+
