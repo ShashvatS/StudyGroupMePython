c72017613d71eec23a4485dfad06cd827d6450f1
Shashvat Srivastava <shashvat.sri@gmail.com>
2019-10-19 23:41:11 -0400

    cors support

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ def create_group(groupname):
 
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/<group>', methods=['POST'])
 def main(group):
